e9752826c5eac7e4ff46c621a3c6771cd36dd3cd
Abhishek Saxena2 <abhisheksaxena2@M-C56R6F7MCW.local>
2024-02-07 15:06:45 +0530

    lints-resolve

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:"export",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "media.licdn.com",
-      },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "media.licdn.com",
+  //     },
+  //   ],
+  // },
+  // experimental: {
+  //   serverActions: true,
+  // },
 };
 
 module.exports = nextConfig;
